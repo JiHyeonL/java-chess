74d9161c6aa4449fd723981027ebffc405c62700
lemone <lejinhy@gmail.com>
2024-04-03 14:41:25 +0900

    refactor: optional 대신 orElseThrow 사용

--- src/main/java/chess/domain/score/Score.java
+++ src/main/java/chess/domain/score/Score.java
@@ -2,10 +2,7 @@ package chess.domain.score;
 
 import chess.domain.piece.PieceType;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 public enum Score {
     QUEEN(PieceType.QUEEN, 9),
@@ -26,14 +23,11 @@ public enum Score {
     }
 
     public static double value(PieceType pieceType) {
-        Optional<Score> values =  Arrays.stream(Score.values())
+        Score values = Arrays.stream(Score.values())
                 .filter(score -> score.pieceType.equals(pieceType))
-                .findFirst();
+                .findAny()
+                .orElseThrow(() ->  new NullPointerException("pieceType에 맞는 score가 없습니다."));
 
-        if (values.isPresent()) {
-            return values.get().score;
-        }
-
-        throw new NullPointerException("pieceType에 맞는 score가 없습니다.");
+        return values.score;
     }
 }
